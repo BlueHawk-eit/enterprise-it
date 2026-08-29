2140771f6ad5bf9d3c7022e15259345bfb7b2c61
BlueHawk-eit <alimubashir4567am@gmail.com>
2026-08-29 15:30:04 +0930

    Add alias for admin middleware

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -17,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(append: [
             \Illuminate\Http\Middleware\HandleCors::class,
         ]);
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
